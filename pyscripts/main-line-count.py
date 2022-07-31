0624e515736371702518608e42bb3814458f365e
OctavioBit <elgorilondelomas@hotmail.com>
2022-07-31 00:35:22 -0300

    commit final

--- pyscripts/main-line-count.py
+++ pyscripts/main-line-count.py
@@ -21,8 +21,14 @@ def main():
     #stDirectory = "F:\\data\\Tesis\\EvolucionAprendizajeTDD\\pyscripts\\1C2021\\stParciales1C2021\\"
     #csvFile = open('LineCountParciales1C2021.csv', 'w', encoding='UTF8', newline='')
 
-    stDirectory = "F:\\data\\Tesis\\EvolucionAprendizajeTDD\\pyscripts\\1C2021\\stSolucionesParcial1C2021\\"
-    csvFile = open('LineCountSolParcial1C2021.csv', 'w', encoding='UTF8', newline='')
+    #stDirectory = "F:\\data\\Tesis\\EvolucionAprendizajeTDD\\pyscripts\\1C2021\\stSolucionesParcial1C2021\\"
+    #csvFile = open('LineCountSolParcial1C2021.csv', 'w', encoding='UTF8', newline='')
+
+    stDirectory = "F:\\data\\Tesis\\EvolucionAprendizajeTDD\\pyscripts\\2C2020\\stParciales2C2020\\"
+    csvFile = open('LineCountParciales2C2020.csv', 'w', encoding='UTF8', newline='')
+
+    #stDirectory = "F:\\data\\Tesis\\EvolucionAprendizajeTDD\\pyscripts\\2C2020\\stSolucionesParcial2C2020\\"
+    #csvFile = open('LineCountSolParcial2C2020.csv', 'w', encoding='UTF8', newline='')
     
     csvWriter = csv.writer(csvFile)
     
